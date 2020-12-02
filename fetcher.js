4a62c16e79a31e0340dbc73e7894e2c9b78fec61
Joseph Williams <josephwilliamsjs@gmail.com>
2020-12-02 22:28:11 +0000

    writes body of url into local file path

--- fetcher.js
+++ fetcher.js
@@ -1,4 +1,3 @@
-// to dos
 const process = require('process');
 const request = require('request');
 const fs = require('fs');
@@ -9,15 +8,22 @@ const url = args[0];
 const localFilePath = args[1];
 
 // Use the request library to make the HTTP request
-request(url, function (error, response, body) {  
-  // Use Node's fs module to write the file
-  const data = body;
-  fs.writeFile(localFilePath, data, (err) => {
-    if (err) throw err;
-    console.log('the file was saved');
-  })
-
-})
+request(url, (error, response, body) => {
+  if (error) {
+    throw error;
+  } else {
+    // Use Node's fs module to write the file
+    const data = body;
+    fs.writeFile(localFilePath, data, (error) => {
+      if (error) {
+        throw error;
+      } else {
+        // data.length === bytes of body
+        console.log(`Downloaded and saved ${data.length} bytes to ${localFilePath}`);
+      }
+    });
+  }
+});
 
 
 
